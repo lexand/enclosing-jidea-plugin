9e89414b0462ab8fd1ad299ecebe5b5dc006bda4
Alexandr <dorogikh.alexander@gmail.com>
2013-04-30 14:14:38 +0300

    - first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Enclosing Plugin for IntelliJ IDEA
+==================================
+
+Previously I was working with NetBeans IDE. And it has very simple very useful behavior in editor:
+
+You just select some portion of text and press one of predefined character **"** **(** **{** **[** **'** **`** and your
+selection will be enclosed with typed character.
+
+* Plugin does not work in "Column selection mode".
+* Compatible with other JetBrains products
---
+++ src/com/lexand/enclosing/EnclosingMod.java
@@ -0,0 +1,100 @@
+package com.lexand.enclosing;
+
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.SelectionModel;
+import com.intellij.openapi.editor.actionSystem.EditorActionManager;
+import com.intellij.openapi.editor.actionSystem.TypedAction;
+import com.intellij.openapi.editor.actionSystem.TypedActionHandler;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleComponent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+
+/**
+ * Quotes
+ *
+ * @author alex
+ * @date 30.04.13
+ * @time 11:01
+ * @package com.lexand.enclosing
+ */
+public class EnclosingMod implements ModuleComponent {
+    public EnclosingMod(Module module) {
+    }
+
+    public void initComponent() {
+        // insert component initialization logic here
+    }
+
+    public void disposeComponent() {
+        // insert component disposal logic here
+    }
+
+    @NotNull
+    public String getComponentName() {
+        return "Selection Enclose Plugin";
+    }
+
+    public void projectOpened() {
+        // called when project is opened
+    }
+
+    public void projectClosed() {
+        // called when project is being closed
+    }
+
+    public void moduleAdded() {
+        EditorActionManager manager = EditorActionManager.getInstance();
+        TypedAction typedAction = manager.getTypedAction();
+        typedAction.setupHandler(new EncloseTypedAction(typedAction.getHandler()));
+    }
+
+    private class EncloseTypedAction implements TypedActionHandler {
+
+        private final TypedActionHandler defaultHandler;
+        private final char[] CHARS = {'"', '\'', '(', '[', '`', '{'};
+        private final char[] PAIRED_CHARS = {'(', ')', '[', ']', '{', '}'};
+
+        public EncloseTypedAction(TypedActionHandler handler) {
+            defaultHandler = handler;
+        }
+
+        @Override
+        public void execute(@NotNull Editor editor, char charTyped, @NotNull DataContext dataContext) {
+            Document document = editor.getDocument();
+
+            SelectionModel selectionModel = editor.getSelectionModel();
+
+            if (document.isWritable() &&
+                    (Arrays.binarySearch(CHARS, charTyped) >= 0) &&
+                    selectionModel.hasSelection() &&
+                    !selectionModel.hasBlockSelection()
+                    ) {
+
+                CaretModel caretModel = editor.getCaretModel();
+
+                int pairInd = Arrays.binarySearch(PAIRED_CHARS, charTyped) + 1;
+                char paired = (pairInd > 0) ? PAIRED_CHARS[pairInd] : charTyped;
+
+                int startSelection = selectionModel.getSelectionStart();
+                int endSelection = selectionModel.getSelectionEnd();
+
+                document.replaceString(
+                        startSelection,
+                        endSelection,
+                        charTyped + selectionModel.getSelectedText() + paired
+                );
+
+                caretModel.moveToOffset(endSelection + 2);
+
+            }
+            else {
+                defaultHandler.execute(editor, charTyped, dataContext);
+            }
+        }
+    }
+}
